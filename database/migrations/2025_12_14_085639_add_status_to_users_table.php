e08eb01ef3be97a22d9be9857239182155117ff9
Anh Dung <23050155@student.bdu.edu.vn>
2025-12-20 08:06:13 +0700

    cap nhat

--- database/migrations/2025_12_14_085639_add_status_to_users_table.php
+++ database/migrations/2025_12_14_085639_add_status_to_users_table.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->tinyInteger('status')->default(1)->after('role');
-        });
+        // status đã được thêm ở migration khác
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('status');
-        });
+        //
     }
 };
