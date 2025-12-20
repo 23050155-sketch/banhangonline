51a24390a16269f7f9b4c285472f5bd6fa22a73e
Anh Dung <23050155@student.bdu.edu.vn>
2025-12-20 07:52:45 +0700

    fix1

--- database/migrations/2025_12_14_085253_add_role_phone_address_status_to_users_table.php
+++ database/migrations/2025_12_14_085253_add_role_phone_address_status_to_users_table.php
@@ -6,24 +6,47 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('customer');
-            $table->tinyInteger('status')->default(1); // 1=active, 0=blocked
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
+
+            if (!Schema::hasColumn('users', 'role')) {
+                $table->string('role')->default('customer');
+            }
+
+            if (!Schema::hasColumn('users', 'status')) {
+                $table->tinyInteger('status')->default(1); // 1=active, 0=blocked
+            }
+
+            if (!Schema::hasColumn('users', 'phone')) {
+                $table->string('phone')->nullable();
+            }
+
+            if (!Schema::hasColumn('users', 'address')) {
+                $table->string('address')->nullable();
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['role','status','phone','address']);
+
+            if (Schema::hasColumn('users', 'address')) {
+                $table->dropColumn('address');
+            }
+
+            if (Schema::hasColumn('users', 'phone')) {
+                $table->dropColumn('phone');
+            }
+
+            if (Schema::hasColumn('users', 'status')) {
+                $table->dropColumn('status');
+            }
+
+            if (Schema::hasColumn('users', 'role')) {
+                $table->dropColumn('role');
+            }
         });
     }
-
 };
